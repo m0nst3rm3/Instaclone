8ddc93bf21b9253f29f8bbbdb54660d348464c4f
SantoshSan <nishantsingh.sudeep@gmail.com>
2020-10-18 20:55:43 +0545

    Start

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
